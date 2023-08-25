4ef4dc56881dc8d54b022bab545f9c3c78363263
danielkiesshau <danielkiesshau@outlook.com>
2023-08-25 16:46:58 -0300

    refactor: extract SelectedImage component

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -4,26 +4,5 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#FFF"
-  },
-  description: {
-    color: "#2E9D4C",
-    fontFamily: "Poppins_400Regular",
-    textAlign: "center",
-    alignSelf: 'center',
-    fontSize: 14,
-    textAlignVertical: "center"
-  },
-  content: {
-    flex: 1,
-  },
-  imageContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  image: {
-    flex: 1,
-    height: '100%',
-    width: '100%',
-  },
+  }
 });
